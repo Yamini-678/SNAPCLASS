e598b521e37d7b9f16f2c4026fb8dce82375bce7
Yamini-678 <yaminibisht678@gmail.com>
2026-05-02 14:00:30 +0530

    first commit

--- src/components/footer.py
+++ src/components/footer.py
@@ -1,19 +1,23 @@
 import streamlit as st
 
+
 def footer_home():
     
-    logo_url = 'https://i.ibb.co/4r5X1FY/apnacollege.png'
-    st.markdown(
-        f"""
-        <div style="margin-top:2rem;
-            display:flex;
-            gap:6px;
-            justify-content:center;
-            items-align:center"
-        >
-        <p style='font-weight:bold; color:white;'>Created With ❤️ by</p>
-        <p style='max-height:25px;font-weight:bold; color:white'>Yamini</p>
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by Yamini </p>  
+        
         </div>
-               
-                """ , unsafe_allow_html=True
-    )
+                
+                """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by Yamini</p>  
+
+        </div>
+                
+                """, unsafe_allow_html=True)
